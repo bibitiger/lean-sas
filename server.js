dde62406a7f2606e45be8133fd73d57b5fd086e2
fanxu1986 <fan_xu1986@hotmail.com>
2016-05-27 15:31:56 +0800

    appid...

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ AV.init({
   appId: '1UlsKsiUTHpNkAyAKSWVW1oo-gzGzoHsz',
   appKey: 'MeyXCB3GkeYmQkQFOacuTSMU',
   masterkey: 'jd2JpdnV8fHXiMW5bBimdlTT'
-})
+});
 
 // 如果不希望使用 masterKey 权限，可以将下面一行删除
 AV.Cloud.useMasterKey();
