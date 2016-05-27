be271c4deb825c6ffa8f856b3cc9dad5cfbd09ed
fanxu1986 <fan_xu1986@hotmail.com>
2016-05-27 18:57:29 +0800

    log params

--- cloud.js
+++ cloud.js
@@ -4,7 +4,7 @@ var AV = require('leanengine');
  * 一个简单的云代码方法
  */
 AV.Cloud.define('hello', function(request, response) {
-	console.log("func hello "+request.toJson());
+	console.log("func hello "+JSON.stringify(request.params));
   	response.success('Hello world!');
 });
 
