71fac859cf5ca536f36715da35d2ec56ec53d205
fanxu1986 <fan_xu1986@hotmail.com>
2016-05-30 16:22:17 +0800

    test request LC-Session

--- cloud.js
+++ cloud.js
@@ -1,3 +1,9 @@
+/**
+ * @Author   bibitiger
+ * @DateTime 2016-05-30T15:13:19+0800
+ * @description define cloud functions for 呼吸康test by avoscloud
+ */
+
 var AV = require('leanengine');
 
 /**
@@ -5,7 +11,23 @@ var AV = require('leanengine');
  */
 AV.Cloud.define('hello', function(request, response) {
 	console.log("func hello "+JSON.stringify(request.params));
+	if(request.user){
+		console.log("user is "+user.getUsername());
+	}else{
+		console.log("user is nil");
+	}
+	
   	response.success('Hello world!');
 });
+
+/**
+ * @Author   bibitiger
+ * @DateTime 2016-05-30T15:13:19+0800
+ * @description for client to request a doctor due with report, if by now no useful doc will return response with "no useful doctor"
+ */
+AV.Cloud.define('RquestDoctor', function(request, response) {
+
+});
+
 
 module.exports = AV.Cloud;
