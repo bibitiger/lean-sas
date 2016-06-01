878ce043ea51ee3797fef794180cc5d0be1b748f
fanxu1986 <fan_xu1986@hotmail.com>
2016-06-01 15:57:44 +0800

    move phoneCheckCode register login boundWX boundPhone to local deploy and github

--- cloud.js
+++ cloud.js
@@ -80,7 +80,7 @@ AV.Cloud.define('RquestDoctor', function(request, response) {
 									//set acl to doc and patient
 									var groupACL = new AV.ACL();
 									groupACL.setReadAccess(listDoc[loc].get('CreateBy'), true);
-									groupACL.setWriteAccess(listDoc[loc].get('CreateBy'), true);
+									groupACL.setWriteAccess(listDoc[loc].get('CreateBy'), false);
 									groupACL.setReadAccess(request.user, true);
 									groupACL.setWriteAccess(request.user, true);
 									listReport[0].setACL(groupACL);
@@ -206,6 +206,251 @@ AV.Cloud.define('WXLogin', function(request, response) {
 	  console.log(error);
 	  response.error(error);
 	});
+});
+
+/**
+ * @Author   bibitiger
+ * @DateTime 2016-06-01T15:07:50+0800
+ * @description for client to Login with phone
+ */
+AV.Cloud.define('login', function(request, response) {
+	var phoneNumber = request.params.phoneNumber;
+	var password = request.params.password;
+
+	// console.log(phoneNumber + '---' + password);
+
+	var data = {};
+
+	AV.User.logInWithMobilePhone(phoneNumber, password).then(function(user) {
+	  //登录成功
+	    var query = new AV.Query('Patients');
+	    query.equalTo('user', user);
+	    query.find().then(function(results) {
+	        
+	        if(results.length > 0){
+	                var profile = results[0];
+	                
+	                data['userId'] = user.id;
+	                data['profileId'] = profile.id;
+	                data['sessionToken'] = user._sessionToken;
+	                data['user'] = user;
+	                //...
+	                response.success(data);            
+	        }else{
+	                var Patient = AV.Object.extend('Patients');
+	                var patient = new Patient();
+	                patient.set('user',user)
+	                
+	                 // 新建一个 ACL 实例
+	                var acl = new AV.ACL();
+	                acl.setPublicReadAccess(true);
+	                acl.setWriteAccess(user,true);
+	                  // 将 ACL 实例赋予 patient 对象
+	                patient.setACL(acl);
+	            
+	                patient.save().then(function(patient) {
+	                    
+	                    data['userId'] = user.id;
+	                    data['profileId'] = patient.id;
+	                    data['sessionToken'] = user._sessionToken;
+	                    data['user'] = user;
+	                    //...
+	                    response.success(data); 
+	                }, function(err) {
+	                    
+	                    console.log('Failed to create new object, with error message: ' + err.message);
+	                    response.error(err);
+	                });
+	        }
+
+	    }, function(error) {
+	        console.log('Error: ' + error.code + ' ' + error.message);
+	    });
+	  
+	}, function(err) {
+	  //登录失败
+	  response.error(err);
+	});
+});
+
+/**
+ * @Author   bibitiger
+ * @DateTime 2016-06-01T15:07:50+0800
+ * @description 
+ */
+AV.Cloud.define('phoneCheckCode', function(request, response) {
+
+	var phoneNumber = request.params.phoneNumber;
+
+	var query = new AV.Query(AV.User);
+	query.equalTo('mobilePhoneNumber', phoneNumber); 
+	query.find().then(function(results) {
+	  if(results.length > 0){
+	      response.error('该手机号已被注册');
+	  }else{
+	     
+	    AV.Cloud.requestSmsCode(phoneNumber).then(function() {
+	      //发送成功
+	      response.success(phoneNumber);
+	    }, function(err) {
+	      //发送失败
+	        response.error(err);
+	    });
+	 
+	  }
+	}, function(err) {
+	  //发送失败
+	    response.error(err);
+	});
+});
+
+/**
+ * @Author   bibitiger
+ * @DateTime 2016-06-01T15:07:50+0800
+ * @description 
+ */
+AV.Cloud.define('register', function(request, response) {
+	//登录或者注册使用同一个接口
+
+	var phoneNumber = request.params.phoneNumber;
+	var password = request.params.password;
+	var checkCode = request.params.checkCode;
+
+
+	var data = {};
+
+	var user = new AV.User();
+	user.signUpOrlogInWithMobilePhone({
+	  mobilePhoneNumber: request.params.phoneNumber,
+	  smsCode: request.params.checkCode,
+	  password: request.params.password,
+	}).then(function(user) {
+	    var Patient = AV.Object.extend('Patients');
+	    var patient = new Patient();
+	    patient.set('user',user)
+	    
+	     // 新建一个 ACL 实例
+	    var acl = new AV.ACL();
+	    acl.setPublicReadAccess(true);
+	    acl.setWriteAccess(user,true);
+	      // 将 ACL 实例赋予 patient 对象
+	    patient.setACL(acl);
+
+	    patient.save().then(function(patient) {
+	        
+	        response.success(user);
+	    }, function(err) {
+	        
+	        console.log('Failed to create new object, with error message: ' + err.message);
+	        response.error(err);
+	    });
+	    
+	}, function(error) {
+	  // 失败
+	  console.log(error);
+	  response.error(error);
+	});
+});
+
+/**
+ * @Author   bibitiger
+ * @DateTime 2016-06-01T15:07:50+0800
+ * @description 
+ */
+AV.Cloud.define('boundWX', function(request, response) {
+
+	var sessionToken = request.params.token;
+	var openId = request.params.openId;
+	var access_token = request.params.access_token;
+	var expires_in = request.params.expires_in;
+
+	var query = new AV.Query(AV.User);
+	query.equalTo('WXOpenId', openId); 
+	query.find().then(function(results) {
+	  if(results.length > 0){
+	      response.error('该微信已被其他账号绑定');
+	  }else{
+	        AV.User.become(sessionToken).then(function (user) {
+	          // The current user is changed.
+	            user._linkWith('WeiXin', {
+	                'authData': {
+	                'uid': openId,
+	                'access_token': access_token,
+	                'expires_in': expires_in
+	                }
+	            }).then(function(user) {
+	              //返回绑定后的用户
+	              console.log(user);
+	              
+	              user.set('WXOpenId', openId);
+	              user.save().then(function (user){
+	                response.success(user);
+	              }, function (error){
+	                response.error(error);
+	              });
+	            }, function(error) {
+	              console.log(error);
+	              response.error(error);
+	            });
+	        }, function (error) {
+	          // Login failed.
+	            response.error(error);
+	        });     
+	  }
+	}, function(err) {
+	  //发送失败
+	    response.error(err);
+	});
+
+
+
+
+});
+
+/**
+ * @Author   bibitiger
+ * @DateTime 2016-06-01T15:07:50+0800
+ * @description 
+ */
+AV.Cloud.define('boundPhone', function(request, response) {
+	var sessionToken = request.params.token;
+	var phoneNumber = request.params.phoneNumber;
+
+	var query = new AV.Query(AV.User);
+	query.equalTo('mobilePhoneNumber', phoneNumber); 
+	query.find().then(function(results) {
+	  if(results.length > 0){
+	      response.error('该手机号已绑定其他账号');
+	  }else{
+	        AV.User.become(sessionToken).then(function (user) {
+	          // The current user is changed.
+	          
+	            user.set('mobilePhoneNumber',phoneNumber);
+	            user.save().then(function() {
+	              // 成功
+	              user.set('mobilePhoneVerified',true);
+	              user.save().then(function() {
+	                response.success(user);
+	              }, function (error){
+	                response.error(error);
+	            });
+	        
+	            }, function (error){
+	                response.error(error);
+	            });
+	        }, function (error) {
+	          // Login failed.
+	            response.error(error);
+	        });
+	  }
+	}, function(err) {
+	  //发送失败
+	    response.error(err);
+	});
+
+
+
+
 });
 
 
