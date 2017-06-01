5f7873ccb06df73abb75f7a2c6cf2cbdbab1ed06
fanxu1986 <fan_xu1986@hotmail.com>
2017-06-01 17:20:01 +0800

    web server

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var todos = require('./routes/todos');
+var wechart = require('./routes/wechart');
 var AV = require('leanengine');
 
 var app = express();
@@ -51,6 +52,7 @@ app.get('/', function(req, res) {
 
 // 可以将一类的路由单独保存在一个文件中
 app.use('/todos', todos);
+app.use('/wechart', wechart);
 
 app.use(function(req, res, next) {
   // 如果任何一个路由都没有返回响应，则抛出一个 404 异常给后续的异常处理器
---
+++ routes/wechart.js
@@ -0,0 +1,28 @@
+'use strict';
+var router = require('express').Router();
+var AV = require('leanengine');
+
+// `AV.Object.extend` 方法一定要放在全局变量，否则会造成堆栈溢出。
+// 详见： https://leancloud.cn/docs/js_guide.html#对象
+var Todo = AV.Object.extend('Todo');
+
+// 查询 Todo 列表
+router.get('/', function(req, res, next) {
+  console.log(req.params);
+  console.log(req.query);
+  res.json({test:req.query.test});
+});
+
+// 新增 Todo 项目
+router.post('/', function(req, res, next) {
+  var content = req.body.content;
+  var todo = new Todo();
+  todo.set('content', content);
+  todo.save().then(function(todo) {
+    res.redirect('/todos');
+  }).catch(next);
+});
+
+module.exports = router;
+
+
