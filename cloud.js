a32c5e14326f234bdf34e93e38fa384658b36691
danledian <474608897@qq.com>
2017-06-21 16:53:58 +0800

    测试

--- cloud.js
+++ cloud.js
@@ -230,15 +230,15 @@ AV.Cloud.afterSave('BaseReports', function(request){
 
 			var isEffect = true;
 
-			if(start == "-1" || start.length < 100 || AHI == -1){
+			if(start == "-1" || start.length < 4 || AHI == -1){
 
-				monthDate = "1706";
-				start = 170620230129;
-				end = 1000;
-				AHI = 5;
+				// monthDate = "1706";
+				// start = 170620230129;
+				// end = 1000;
+				// AHI = 5;
 
 				console.log("not effect report");
-				// return;
+				return;
 				
 			}else{
 				monthDate = start.substr(0, 4);
